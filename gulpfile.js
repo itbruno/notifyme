f9b6a69c65a86f72f895496c0e791607d36ad2df
Bruno Rodrigues <brunowebr@gmail.com>
2019-07-22 01:31:55 -0300

    update version for bower and add package to minify plugin js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,23 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
+var uglify = require("gulp-uglify");
+var rename = require("gulp-rename");
 
 var paths = {
 	sass : "assets/scss/",
 	css : "assets/css/",
-	js : "assets/js/"
+	js : "assets/js/notifyme.js"
 };
+
+gulp.task('js-min', function(){
+    gulp.src(paths.js)
+        .pipe(uglify())
+        .on('error', function(err) {
+            console.log('ERROR:', err.message);
+        })
+        .pipe(rename('notifyme.min.js'))
+        .pipe(gulp.dest('assets/js/'))
+});
 
 gulp.task('compile-sass', function(){
 	return gulp.src(paths.sass + "*.scss")
